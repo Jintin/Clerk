8fbdcbffe207b2cc9144a676ef6d17a507d0e0c7
Jintin <jintin.lin1018@gmail.com>
2019-04-21 22:00:34 +0800

    add comment

--- app/src/main/java/com/jintin/clerk/app/utils/ServiceUtils.kt
+++ app/src/main/java/com/jintin/clerk/app/utils/ServiceUtils.kt
@@ -2,6 +2,9 @@ package com.jintin.clerk.app.utils
 
 import android.content.Context
 
+/**
+ * wrapper of getSystemService
+ */
 fun <T> Context.getSystemManager(name: String): T {
     @Suppress("UNCHECKED_CAST")
     return getSystemService(name) as T
